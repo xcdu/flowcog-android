3df8d4a24b07558f5baea4425ee917dc93f27e28
xcdu <czduxuechao@gmail.com>
2017-12-29 03:06:25 +0800

    add original flowcog files

--- src/main/java/com/flowcog/android/Analyze.java
+++
@@ -1,8 +0,0 @@
-package com.flowcog.android;
-
-public class Analyze {
-  public static void main(String[] args) {
-    return;
-  }
-
-}
---
+++ src/main/java/com/flowcog/android/data/parsers/IPermissionMethodParser.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Secure Software Engineering Group at EC SPRIDE.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors: Christian Fritz, Steven Arzt, Siegfried Rasthofer, Eric
+ * Bodden, and others.
+ ******************************************************************************/
+package com.flowcog.android.data.parsers;
+
+import com.flowcog.android.data.AndroidMethod;
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * Common interface for all parsers that are able to read in files with Android
+ * methods and permissions
+ *
+ * @author Steven Arzt
+ *
+ */
+public interface IPermissionMethodParser {
+	
+	Set<AndroidMethod> parse() throws IOException;
+
+}
